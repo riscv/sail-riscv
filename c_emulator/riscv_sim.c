7f813e3ca1e5db52c781ff36477c8ba0336db453
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2019-06-06 17:26:51 -0700

    Remove mmio in rvfi mode.

--- c_emulator/riscv_sim.c
+++ c_emulator/riscv_sim.c
@@ -433,6 +433,9 @@ void init_sail(uint64_t elf_entry)
     rv_ram_size = UINT64_C(0x10000);
     rv_rom_base = UINT64_C(0);
     rv_rom_size = UINT64_C(0);
+    rv_clint_base = UINT64_C(0);
+    rv_clint_size = UINT64_C(0);
+    rv_htif_tohost = UINT64_C(0);
     zPC = elf_entry;
   } else
 #endif
