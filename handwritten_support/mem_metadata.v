4809371012e7394853f01b179d088497212cf480
Thomas Bauereiss <tb592@cl.cam.ac.uk>
2019-11-26 17:32:16 +0000

    Fix RV32 Coq build

--- handwritten_support/mem_metadata.v
+++ handwritten_support/mem_metadata.v
@@ -4,8 +4,8 @@ Require Import Sail2_operators_mwords.
 Require Import Sail2_prompt_monad.
 Require Import Sail2_prompt.
 
-Definition write_ram {rv e} wk (addr : mword 64) size (v : mword (8 * size)) (meta : unit) : monad rv bool e := write_mem wk 64 addr size v.
+Definition write_ram {rv e a} wk (addr : mword a) size (v : mword (8 * size)) (meta : unit) : monad rv bool e := write_mem wk a addr size v.
 
-Definition read_ram {rv e} rk (addr : mword 64) size (read_tag : bool) `{ArithFact (size >= 0)} : monad rv (mword (8 * size) * unit) e :=
-  read_mem rk 64 addr size >>= fun data =>
+Definition read_ram {rv e a} rk (addr : mword a) size (read_tag : bool) `{ArithFact (size >= 0)} : monad rv (mword (8 * size) * unit) e :=
+  read_mem rk a addr size >>= fun data =>
   returnm (data, tt).
