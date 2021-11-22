085cfcd3fc3be92aaa86b4caabf9d248eae5f83e
Jessica Clarke <jrtc27@jrtc27.com>
2021-11-22 18:02:40 +0000

    Implement support for Zfinx (#130)
    NB: Smstateen support is missing in the model so enabling the Zfinx
    extension provides an architectural covert channel via FCSR if
    privileged software is not aware of Zfinx's existence.
    
    Co-authored-by: Jessica Clarke <jrtc27@jrtc27.com>
    
    Co-authored-by: Ibrahim Abu Kharmeh <abukharmeh@gmail.com>

--- c_emulator/riscv_platform.h
+++ c_emulator/riscv_platform.h
@@ -4,6 +4,7 @@
 bool sys_enable_rvc(unit);
 bool sys_enable_next(unit);
 bool sys_enable_fdext(unit);
+bool sys_enable_zfinx(unit);
 bool sys_enable_writable_misa(unit);
 
 bool plat_enable_dirty_update(unit);
