5f8b98f1e79046af4c4d266402d834940a121129
Xinlai Wan <xinlai.w@rioslab.org>
2022-12-27 20:23:10 +0800

    V extension general framework and configuration setting instructions (#191)
    * V extension general framework and configuration setting instructions
    
    * Update model/riscv_insts_vext_utils.sail
    
    fix a typo
    
    Co-authored-by: Nicolas Brunie <nibrunie@gmail.com>
    Signed-off-by: BrighterW <xinlai.w@rioslab.org>
    
    * Update model/riscv_insts_vext_vset.sail
    
    * Revisions after Nov 22 meeting
    
    * Update effect matching for functions in riscv_vlen.sail
    
    * Fix code formatting issues
    
    * Update model/riscv_insts_vext_utils.sail
    
    Co-authored-by: Jessica Clarke <jrtc27@jrtc27.com>
    Signed-off-by: Xinlai Wan <xinlai.w@rioslab.org>
    
    * Fix coding style issues
    
    * Update vset instructions
    
    Signed-off-by: BrighterW <xinlai.w@rioslab.org>
    Signed-off-by: Xinlai Wan <xinlai.w@rioslab.org>
    Co-authored-by: Nicolas Brunie <nibrunie@gmail.com>
    Co-authored-by: Jessica Clarke <jrtc27@jrtc27.com>

--- ocaml_emulator/platform.ml
+++ ocaml_emulator/platform.ml
@@ -11,6 +11,7 @@ let config_enable_dirty_update         = ref false
 let config_enable_misaligned_access    = ref false
 let config_mtval_has_illegal_inst_bits = ref false
 let config_enable_pmp                  = ref false
+let config_enable_rvv                  = ref true
 
 let platform_arch = ref P.RV64
 
@@ -78,6 +79,7 @@ let enable_writable_misa ()          = !config_enable_writable_misa
 let enable_rvc ()                    = !config_enable_rvc
 let enable_next ()                   = !config_enable_next
 let enable_fdext ()                  = false
+let enable_rvv ()                    = !config_enable_rvv
 let enable_dirty_update ()           = !config_enable_dirty_update
 let enable_misaligned_access ()      = !config_enable_misaligned_access
 let mtval_has_illegal_inst_bits ()   = !config_mtval_has_illegal_inst_bits
