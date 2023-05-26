be0962c1bdca50ae55ee20ed445c9686956ef5df
Alex Richardson <alexrichardson@google.com>
2023-05-26 07:17:39 -0700

    csim: Allow redirecting trace output to a file using command line flag
    This is useful when booting an operating system with tracing enabled
    as it allows showing the printed messages on the terminal and storing
    the trace log to a separate file. All categorized trace messages are
    now redirected to the --trace-output file when passed. If this option is
    not gived, the current behaviour of using stdout is retained.
    
    Example usage:
    `./c_emulator/riscv_sim_RV64 -b os-boot/rv64-64mb.dtb --trace-output /dev/stderr os-boot/linux-rv64-64mb.bbl --trace-output /tmp/linux.log`

--- c_emulator/riscv_prelude.c
+++ c_emulator/riscv_prelude.c
@@ -1,5 +1,6 @@
 #include "riscv_prelude.h"
 #include "riscv_config.h"
+#include "riscv_platform_impl.h"
 
 unit print_string(sail_string prefix, sail_string msg)
 {
@@ -10,28 +11,28 @@ unit print_string(sail_string prefix, sail_string msg)
 unit print_instr(sail_string s)
 {
   if (config_print_instr)
-    printf("%s\n", s);
+    fprintf(trace_log, "%s\n", s);
   return UNIT;
 }
 
 unit print_reg(sail_string s)
 {
   if (config_print_reg)
-    printf("%s\n", s);
+    fprintf(trace_log, "%s\n", s);
   return UNIT;
 }
 
 unit print_mem_access(sail_string s)
 {
   if (config_print_mem_access)
-    printf("%s\n", s);
+    fprintf(trace_log, "%s\n", s);
   return UNIT;
 }
 
 unit print_platform(sail_string s)
 {
   if (config_print_platform)
-    printf("%s\n", s);
+    fprintf(trace_log, "%s\n", s);
   return UNIT;
 }
 
