3c09a5023a2bf2ef427eaa7f168e6e4dc85b31ca
Ved Shanbhogue <ved@rivosinc.com>
2024-01-20 15:40:22 -0600

    Add Svnapot and Svpbmt extensions

--- c_emulator/riscv_platform_impl.h
+++ c_emulator/riscv_platform_impl.h
@@ -24,6 +24,8 @@ extern bool rv_enable_dirty_update;
 extern bool rv_enable_misaligned;
 extern bool rv_mtval_has_illegal_inst_bits;
 extern bool rv_enable_writable_fiom;
+extern bool rv_enable_svnapot;
+extern bool rv_enable_svpbmt;
 
 extern uint64_t rv_ram_base;
 extern uint64_t rv_ram_size;
