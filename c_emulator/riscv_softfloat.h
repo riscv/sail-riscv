9350ad1e61e0605ef5a35af4849c067ea29b05fb
Jordan Carlin <jordanmcarlin@gmail.com>
2024-04-05 00:55:08 -0700

    Merge branch 'LiuTaowen-Tony/master' into quads
    Update to current conventions (remove effects, compatible with Zfh and Zfa extensions that were added since original version written).
    Still some bugs with the current implementation.
    
    Co-authored-by: LiuTaowen Tony <tony_t_liu@outlook.com>

--- c_emulator/riscv_softfloat.h
+++ c_emulator/riscv_softfloat.h
@@ -15,16 +15,24 @@ unit softfloat_f64sub(mach_bits rm, mach_bits v1, mach_bits v2);
 unit softfloat_f64mul(mach_bits rm, mach_bits v1, mach_bits v2);
 unit softfloat_f64div(mach_bits rm, mach_bits v1, mach_bits v2);
 
+unit softfloat_f128add(mach_bits rm, sail_bits v1, sail_bits v2);
+unit softfloat_f128sub(mach_bits rm, sail_bits v1, sail_bits v2);
+unit softfloat_f128mul(mach_bits rm, sail_bits v1, sail_bits v2);
+unit softfloat_f128div(mach_bits rm, sail_bits v1, sail_bits v2);
+
 unit softfloat_f16muladd(mach_bits rm, mach_bits v1, mach_bits v2,
                          mach_bits v3);
 unit softfloat_f32muladd(mach_bits rm, mach_bits v1, mach_bits v2,
                          mach_bits v3);
 unit softfloat_f64muladd(mach_bits rm, mach_bits v1, mach_bits v2,
                          mach_bits v3);
+unit softfloat_f128muladd(mach_bits rm, sail_bits v1, sail_bits v2,
+                          sail_bits v3);
 
 unit softfloat_f16sqrt(mach_bits rm, mach_bits v);
 unit softfloat_f32sqrt(mach_bits rm, mach_bits v);
 unit softfloat_f64sqrt(mach_bits rm, mach_bits v);
+unit softfloat_f128sqrt(mach_bits rm, sail_bits v);
 
 unit softfloat_f16toi32(mach_bits rm, mach_bits v);
 unit softfloat_f16toui32(mach_bits rm, mach_bits v);
@@ -41,6 +49,11 @@ unit softfloat_f64toui32(mach_bits rm, mach_bits v);
 unit softfloat_f64toi64(mach_bits rm, mach_bits v);
 unit softfloat_f64toui64(mach_bits rm, mach_bits v);
 
+unit softfloat_f128toi32(mach_bits rm, sail_bits v);
+unit softfloat_f128toui32(mach_bits rm, sail_bits v);
+unit softfloat_f128toi64(mach_bits rm, sail_bits v);
+unit softfloat_f128toui64(mach_bits rm, sail_bits v);
+
 unit softfloat_i32tof16(mach_bits rm, mach_bits v);
 unit softfloat_ui32tof16(mach_bits rm, mach_bits v);
 unit softfloat_i64tof16(mach_bits rm, mach_bits v);
@@ -56,6 +69,11 @@ unit softfloat_ui32tof64(mach_bits rm, mach_bits v);
 unit softfloat_i64tof64(mach_bits rm, mach_bits v);
 unit softfloat_ui64tof64(mach_bits rm, mach_bits v);
 
+unit softfloat_i32tof128(mach_bits rm, mach_bits v);
+unit softfloat_ui32tof128(mach_bits rm, mach_bits v);
+unit softfloat_i64tof128(mach_bits rm, mach_bits v);
+unit softfloat_ui64tof128(mach_bits rm, mach_bits v);
+
 unit softfloat_f16tof32(mach_bits rm, mach_bits v);
 unit softfloat_f16tof64(mach_bits rm, mach_bits v);
 unit softfloat_f32tof64(mach_bits rm, mach_bits v);
@@ -63,6 +81,11 @@ unit softfloat_f32tof64(mach_bits rm, mach_bits v);
 unit softfloat_f32tof16(mach_bits rm, mach_bits v);
 unit softfloat_f64tof16(mach_bits rm, mach_bits v);
 unit softfloat_f64tof32(mach_bits rm, mach_bits v);
+
+unit softfloat_f32tof128(mach_bits rm, sail_bits v);
+unit softfloat_f64tof128(mach_bits rm, sail_bits v);
+unit softfloat_f128tof32(mach_bits rm, sail_bits v);
+unit softfloat_f128tof64(mach_bits rm, sail_bits v);
 
 unit softfloat_f16lt(mach_bits v1, mach_bits v2);
 unit softfloat_f16lt_quiet(mach_bits v1, mach_bits v2);
@@ -79,6 +102,9 @@ unit softfloat_f64lt_quiet(mach_bits v1, mach_bits v2);
 unit softfloat_f64le(mach_bits v1, mach_bits v2);
 unit softfloat_f64le_quiet(mach_bits v1, mach_bits v2);
 unit softfloat_f64eq(mach_bits v1, mach_bits v2);
+unit softfloat_f128lt(sail_bits v1, sail_bits v2);
+unit softfloat_f128le(sail_bits v1, sail_bits v2);
+unit softfloat_f128eq(sail_bits v1, sail_bits v2);
 
 unit softfloat_f16roundToInt(mach_bits rm, mach_bits v, bool exact);
 unit softfloat_f32roundToInt(mach_bits rm, mach_bits v, bool exact);
--- ocaml_emulator/softfloat.ml
+++ ocaml_emulator/softfloat.ml
@@ -35,6 +35,18 @@ let f64_mul rm v1 v2 =
 let f64_div rm v1 v2 =
   ()
 
+let f128_add rm v1 v2 =
+  ()
+
+let f128_sub rm v1 v2 =
+  ()
+
+let f128_mul rm v1 v2 =
+  ()
+
+let f128_div rm v1 v2 =
+  ()
+
 let f16_muladd rm v1 v2 v3 =
   ()
 
@@ -44,6 +56,10 @@ let f32_muladd rm v1 v2 v3 =
 let f64_muladd rm v1 v2 v3 =
   ()
 
+let f128_muladd rm v1 v2 v3 =
+  ()
+
+
 let f16_sqrt rm v =
   ()
 
@@ -51,6 +67,9 @@ let f32_sqrt rm v =
   ()
 
 let f64_sqrt rm v =
+  ()
+
+let f128_sqrt rm v =
   ()
 
 let f16_to_i32 rm v =
@@ -143,6 +162,43 @@ let f64_to_f16 rm v =
 let f64_to_f32 rm v =
   ()
 
+let f128_to_i32 rm v =
+  ()
+
+let f128_to_ui32 rm v =
+  ()
+
+let i32_to_f128 rm v =
+  ()
+
+let ui32_to_f128 rm v =
+  ()
+
+let f32_to_f128 rm v =
+  ()
+
+let f128_to_f32 rm v =
+  ()
+
+let f128_to_f64 rm v =
+  ()
+
+let f64_to_f128 rm v =
+  ()
+
+let f128_to_i64 rm v =
+  ()
+
+let i64_to_f128 rm v =
+  ()
+
+let f128_to_ui64 rm v =
+  ()
+
+let ui64_to_f128 rm v =
+  ()
+
+
 let f16_lt v1 v2 =
   ()
 
@@ -186,6 +242,15 @@ let f64_le_quiet v1 v2 =
   ()
 
 let f64_eq v1 v2 =
+  ()
+
+let f128_lt v1 v2 =
+  ()
+
+let f128_le v1 v2 =
+  ()
+
+let f128_eq v1 v2 =
   ()
 
 let f16_round_to_int exact rm v =
