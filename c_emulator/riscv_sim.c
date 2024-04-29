f06a04e876cafc0f8e960cc110a2fe6805b9c29c
Martin Berger <contact@martinfriedrichberger.net>
2024-04-30 00:09:59 +0100

    fixup! Add Svinval extension.

--- c_emulator/riscv_sim.c
+++ c_emulator/riscv_sim.c
@@ -53,6 +53,7 @@ const char *RV32ISA = "RV32IMAC";
 #define OPT_ENABLE_WRITABLE_FIOM 1001
 #define OPT_PMP_COUNT 1002
 #define OPT_PMP_GRAIN 1003
+#define OPT_ENABLE_SVINVAL 10017
 #define OPT_ENABLE_ZCB 10014
 
 static bool do_dump_dts = false;
@@ -146,6 +147,7 @@ static struct option options[] = {
     {"inst-limit",                  required_argument, 0, 'l'                     },
     {"enable-zfinx",                no_argument,       0, 'x'                     },
     {"enable-writable-fiom",        no_argument,       0, OPT_ENABLE_WRITABLE_FIOM},
+    {"enable-svinval",              no_argument,       0, OPT_ENABLE_SVINVAL      },
     {"enable-zcb",                  no_argument,       0, OPT_ENABLE_ZCB          },
 #ifdef SAILCOV
     {"sailcov-file",                required_argument, 0, 'c'                     },
