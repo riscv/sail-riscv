c2bb74d48800247fb05aace771f44ebe410893de
Nathaniel Wesley Filardo <wes.filardo@scisemi.com>
2024-11-15 20:46:49 +0000

    Initial support for E extension

--- build_simulators.sh
+++ build_simulators.sh
@@ -2,6 +2,7 @@
 
 set -e
 : "${DOWNLOAD_GMP:=TRUE}"
-cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDOWNLOAD_GMP="${DOWNLOAD_GMP}"
+: "${RVE:=FALSE}"
+cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDOWNLOAD_GMP="${DOWNLOAD_GMP}" -DRVE="${RVE}"
 jobs=$( (nproc || sysctl -n hw.ncpu || echo 2) 2>/dev/null)
 cmake --build build -j${jobs}
