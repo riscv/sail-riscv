2d6d493e0c80886c72c05be6b46c5833a4103c0b
Alex Richardson <alexrichardson@google.com>
2025-02-11 17:37:12 -0800

    Add a DOWNLOAD_GMP option to build against a local GMP build
    This downloads the latest release of GMP and builds it as part of the
    overall emulator build. This should make it easier to build sail without
    having to install system dependencies.
    
    Also change build_simulators.sh to download GMP by default to make it
    easier for users that don't manually configure the CMake build.
    
    See https://github.com/riscv/sail-riscv/issues/215

--- build_simulators.sh
+++ build_simulators.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-
-cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo
+: "${DOWNLOAD_GMP:=TRUE}"
+cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDOWNLOAD_GMP="${DOWNLOAD_GMP}"
 cmake --build build -j2
