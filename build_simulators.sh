08c2ae69ffcf7a256f63abf38acb985b2a7c0abe
Alex Richardson <alexrichardson@google.com>
2025-02-14 10:51:08 -0800

    build_simulators.sh: detect number of jobs for building
    Now that we default to building libgmp, it makes more sense to increase
    job count to number of available CPUs since there is more parallelism.

--- build_simulators.sh
+++ build_simulators.sh
@@ -3,4 +3,5 @@
 set -e
 : "${DOWNLOAD_GMP:=TRUE}"
 cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDOWNLOAD_GMP="${DOWNLOAD_GMP}"
-cmake --build build -j2
+jobs=$( (nproc || sysctl -n hw.ncpu || echo 2) 2>/dev/null)
+cmake --build build -j${jobs}
