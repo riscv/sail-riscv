fee3cf6d92482fec76e742c8053205c0b7360e3e
Jakob von Raumer <jakob@von-raumer.de>
2025-02-24 09:52:05 +0100

    inline float types

--- handwritten_support/RiscvExtras.lean
+++ handwritten_support/RiscvExtras.lean
@@ -3,7 +3,10 @@ import THE_MODULE_NAME.Sail.Sail
 
 open Sail
 
+def print (_ : String) : Unit := ()
 def print_endline (_ : String) : Unit := ()
+def print_bits (_ : String) (_ : BitVec n) : Unit := ()
+def print_string (_ : String) (_ : String) : Unit := ()
 def prerr_endline (_: String) : Unit := ()
 def prerr_string (_: String) : Unit := ()
 def putchar {T} (_: T ) : Unit := ()
@@ -51,86 +54,88 @@ axiom plat_clint_base : Unit → Arch.pa
 axiom plat_clint_size : Unit → Arch.pa
 axiom plat_insns_per_tick : Unit → Int
 
--- TODO import Sail module
-axiom plat_term_write {α β} : α → β
-axiom plat_term_read {α} : Unit → α
+section Effectful
+
+variable {Register : Type} {RegisterType : Register → Type} [DecidableEq Register] [Hashable Register]
+
+axiom plat_term_write {α} : α → PreSailM RegisterType c ue Unit
+axiom plat_term_read : Unit → PreSailM RegisterType c ue String
+
+-- Reservations
+axiom load_reservation : Arch.pa → PreSailM RegisterType c ue Unit
+axiom match_reservation : Arch.pa → PreSailM RegisterType c ue Bool
+axiom cancel_reservation : Unit → PreSailM RegisterType c ue Bool
+
+axiom get_16_random_bits : Unit → PreSailM RegisterType c ue (BitVec 16)
+
+axiom speculate_conditional : Unit → PreSailM RegisterType c ue Bool
+
+end Effectful
 
 -- Floats
-abbrev bits_rm := BitVec 3  -- Rounding mode
-abbrev bits_fflags := BitVec 5  -- Accrued exceptions: NV,DZ,OF,UF,NX
-abbrev bits_H := BitVec 16  -- Half-precision float value
-abbrev bits_S := BitVec 32  -- Single-precision float value
-abbrev bits_D := BitVec 64  -- Double-precision float value
-
-abbrev bits_W := BitVec 32  -- Signed integer
-abbrev bits_WU := BitVec 32  -- Unsigned integer
-
-abbrev bits_L := BitVec 64  -- Signed integer
-abbrev bits_LU := BitVec 64  -- Unsigned integer
-
-axiom extern_f16Add : bits_rm → bits_H → bits_H → Unit
-axiom extern_f16Sub : bits_rm → bits_H → bits_H → Unit
-axiom extern_f16Mul : bits_rm → bits_H → bits_H → Unit
-axiom extern_f16Div : bits_rm → bits_H → bits_H → Unit
-axiom extern_f32Add : bits_rm → bits_S → bits_S → Unit
-axiom extern_f32Sub : bits_rm → bits_S → bits_S → Unit
-axiom extern_f32Mul : bits_rm → bits_S → bits_S → Unit
-axiom extern_f32Div : bits_rm → bits_S → bits_S → Unit
-axiom extern_f64Add : bits_rm → bits_D → bits_D → Unit
-axiom extern_f64Sub : bits_rm → bits_D → bits_D → Unit
-axiom extern_f64Mul : bits_rm → bits_D → bits_D → Unit
-axiom extern_f64Div : bits_rm → bits_D → bits_D → Unit
-axiom extern_f16MulAdd : bits_rm → bits_H → bits_H → bits_H → Unit
-axiom extern_f32MulAdd : bits_rm → bits_S → bits_S → bits_S → Unit
-axiom extern_f64MulAdd : bits_rm → bits_D → bits_D → bits_D → Unit
-axiom extern_f16Sqrt : bits_rm → bits_H → Unit
-axiom extern_f32Sqrt : bits_rm → bits_S → Unit
-axiom extern_f64Sqrt : bits_rm → bits_D → Unit
-axiom extern_f16ToI32 : bits_rm → bits_H → Unit
-axiom extern_f16ToUi32 : bits_rm → bits_H → Unit
-axiom extern_i32ToF16 : bits_rm → bits_W → Unit
-axiom extern_ui32ToF16 : bits_rm → bits_WU → Unit
-axiom extern_f16ToI64 : bits_rm → bits_H → Unit
-axiom extern_f16ToUi64 : bits_rm → bits_H → Unit
-axiom extern_i64ToF16 : bits_rm → bits_L → Unit
-axiom extern_ui64ToF16 : bits_rm → bits_L → Unit
-axiom extern_f32ToI32 : bits_rm → bits_S → Unit
-axiom extern_f32ToUi32 : bits_rm → bits_S → Unit
-axiom extern_i32ToF32 : bits_rm → bits_W → Unit
-axiom extern_ui32ToF32 : bits_rm → bits_WU → Unit
-axiom extern_f32ToI64 : bits_rm → bits_S → Unit
-axiom extern_f32ToUi64 : bits_rm → bits_S → Unit
-axiom extern_i64ToF32 : bits_rm → bits_L → Unit
-axiom extern_ui64ToF32 : bits_rm → bits_L → Unit
-axiom extern_f64ToI32 : bits_rm → bits_D → Unit
-axiom extern_f64ToUi32 : bits_rm → bits_D → Unit
-axiom extern_i32ToF64 : bits_rm → bits_W → Unit
-axiom extern_ui32ToF64 : bits_rm → bits_WU → Unit
-axiom extern_f64ToI64 : bits_rm → bits_D → Unit
-axiom extern_f64ToUi64 : bits_rm → bits_D → Unit
-axiom extern_i64ToF64 : bits_rm → bits_L → Unit
-axiom extern_ui64ToF64 : bits_rm → bits_LU → Unit
-axiom extern_f16ToF32 : bits_rm → bits_H → Unit
-axiom extern_f16ToF64 : bits_rm → bits_H → Unit
-axiom extern_f32ToF64 : bits_rm → bits_S → Unit
-axiom extern_f32ToF16 : bits_rm → bits_S → Unit
-axiom extern_f64ToF16 : bits_rm → bits_D → Unit
-axiom extern_f64ToF32 : bits_rm → bits_D → Unit
-axiom extern_f16Lt : bits_H → bits_H → Unit
-axiom extern_f16Lt_quiet : bits_H → bits_H → Unit
-axiom extern_f16Le : bits_H → bits_H → Unit
-axiom extern_f16Le_quiet : bits_H → bits_H → Unit
-axiom extern_f16Eq : bits_H → bits_H → Unit
-axiom extern_f32Lt : bits_S → bits_S → Unit
-axiom extern_f32Lt_quiet : bits_S → bits_S → Unit
-axiom extern_f32Le : bits_S → bits_S → Unit
-axiom extern_f32Le_quiet : bits_S → bits_S → Unit
-axiom extern_f32Eq : bits_S → bits_S → Unit
-axiom extern_f64Lt : bits_D → bits_D → Unit
-axiom extern_f64Lt_quiet : bits_D → bits_D → Unit
-axiom extern_f64Le : bits_D → bits_D → Unit
-axiom extern_f64Le_quiet : bits_D → bits_D → Unit
-axiom extern_f64Eq : bits_D → bits_D → Unit
-axiom extern_f16roundToInt : bits_rm → bits_H → Bool → Unit
-axiom extern_f32roundToInt : bits_rm → bits_S → Bool → Unit
-axiom extern_f64roundToInt : bits_rm → bits_D → Bool → Unit
+axiom extern_f16Add : BitVec 3 → BitVec 16 → BitVec 16 → Unit
+axiom extern_f16Sub : BitVec 3 → BitVec 16 → BitVec 16 → Unit
+axiom extern_f16Mul : BitVec 3 → BitVec 16 → BitVec 16 → Unit
+axiom extern_f16Div : BitVec 3 → BitVec 16 → BitVec 16 → Unit
+axiom extern_f32Add : BitVec 3 → BitVec 32 → BitVec 32 → Unit
+axiom extern_f32Sub : BitVec 3 → BitVec 32 → BitVec 32 → Unit
+axiom extern_f32Mul : BitVec 3 → BitVec 32 → BitVec 32 → Unit
+axiom extern_f32Div : BitVec 3 → BitVec 32 → BitVec 32 → Unit
+axiom extern_f64Add : BitVec 3 → BitVec 64 → BitVec 64 → Unit
+axiom extern_f64Sub : BitVec 3 → BitVec 64 → BitVec 64 → Unit
+axiom extern_f64Mul : BitVec 3 → BitVec 64 → BitVec 64 → Unit
+axiom extern_f64Div : BitVec 3 → BitVec 64 → BitVec 64 → Unit
+axiom extern_f16MulAdd : BitVec 3 → BitVec 16 → BitVec 16 → BitVec 16 → Unit
+axiom extern_f32MulAdd : BitVec 3 → BitVec 32 → BitVec 32 → BitVec 32 → Unit
+axiom extern_f64MulAdd : BitVec 3 → BitVec 64 → BitVec 64 → BitVec 64 → Unit
+axiom extern_f16Sqrt : BitVec 3 → BitVec 16 → Unit
+axiom extern_f32Sqrt : BitVec 3 → BitVec 32 → Unit
+axiom extern_f64Sqrt : BitVec 3 → BitVec 64 → Unit
+axiom extern_f16ToI32 : BitVec 3 → BitVec 16 → Unit
+axiom extern_f16ToUi32 : BitVec 3 → BitVec 16 → Unit
+axiom extern_i32ToF16 : BitVec 3 → BitVec 32 → Unit
+axiom extern_ui32ToF16 : BitVec 3 → BitVec 32 → Unit
+axiom extern_f16ToI64 : BitVec 3 → BitVec 16 → Unit
+axiom extern_f16ToUi64 : BitVec 3 → BitVec 16 → Unit
+axiom extern_i64ToF16 : BitVec 3 → BitVec 64 → Unit
+axiom extern_ui64ToF16 : BitVec 3 → BitVec 64 → Unit
+axiom extern_f32ToI32 : BitVec 3 → BitVec 32 → Unit
+axiom extern_f32ToUi32 : BitVec 3 → BitVec 32 → Unit
+axiom extern_i32ToF32 : BitVec 3 → BitVec 32 → Unit
+axiom extern_ui32ToF32 : BitVec 3 → BitVec 32 → Unit
+axiom extern_f32ToI64 : BitVec 3 → BitVec 32 → Unit
+axiom extern_f32ToUi64 : BitVec 3 → BitVec 32 → Unit
+axiom extern_i64ToF32 : BitVec 3 → BitVec 64 → Unit
+axiom extern_ui64ToF32 : BitVec 3 → BitVec 64 → Unit
+axiom extern_f64ToI32 : BitVec 3 → BitVec 64 → Unit
+axiom extern_f64ToUi32 : BitVec 3 → BitVec 64 → Unit
+axiom extern_i32ToF64 : BitVec 3 → BitVec 32 → Unit
+axiom extern_ui32ToF64 : BitVec 3 → BitVec 32 → Unit
+axiom extern_f64ToI64 : BitVec 3 → BitVec 64 → Unit
+axiom extern_f64ToUi64 : BitVec 3 → BitVec 64 → Unit
+axiom extern_i64ToF64 : BitVec 3 → BitVec 64 → Unit
+axiom extern_ui64ToF64 : BitVec 3 → BitVec 64 → Unit
+axiom extern_f16ToF32 : BitVec 3 → BitVec 16 → Unit
+axiom extern_f16ToF64 : BitVec 3 → BitVec 16 → Unit
+axiom extern_f32ToF64 : BitVec 3 → BitVec 32 → Unit
+axiom extern_f32ToF16 : BitVec 3 → BitVec 32 → Unit
+axiom extern_f64ToF16 : BitVec 3 → BitVec 64 → Unit
+axiom extern_f64ToF32 : BitVec 3 → BitVec 64 → Unit
+axiom extern_f16Lt : BitVec 16 → BitVec 16 → Unit
+axiom extern_f16Lt_quiet : BitVec 16 → BitVec 16 → Unit
+axiom extern_f16Le : BitVec 16 → BitVec 16 → Unit
+axiom extern_f16Le_quiet : BitVec 16 → BitVec 16 → Unit
+axiom extern_f16Eq : BitVec 16 → BitVec 16 → Unit
+axiom extern_f32Lt : BitVec 32 → BitVec 32 → Unit
+axiom extern_f32Lt_quiet : BitVec 32 → BitVec 32 → Unit
+axiom extern_f32Le : BitVec 32 → BitVec 32 → Unit
+axiom extern_f32Le_quiet : BitVec 32 → BitVec 32 → Unit
+axiom extern_f32Eq : BitVec 32 → BitVec 32 → Unit
+axiom extern_f64Lt : BitVec 64 → BitVec 64 → Unit
+axiom extern_f64Lt_quiet : BitVec 64 → BitVec 64 → Unit
+axiom extern_f64Le : BitVec 64 → BitVec 64 → Unit
+axiom extern_f64Le_quiet : BitVec 64 → BitVec 64 → Unit
+axiom extern_f64Eq : BitVec 64 → BitVec 64 → Unit
+axiom extern_f16roundToInt : BitVec 3 → BitVec 16 → Bool → Unit
+axiom extern_f32roundToInt : BitVec 3 → BitVec 32 → Bool → Unit
+axiom extern_f64roundToInt : BitVec 3 → BitVec 64 → Bool → Unit
