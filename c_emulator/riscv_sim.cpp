047afbeb6c3e3e10e719ca3e9c59fb01d3941961
Tim Hutt <timothy.hutt@codasip.com>
2025-04-28 16:34:44 +0100

    Change XLEN and FLEN to be configure-time options
    This means you only need to compile the emulator once in order to support RV32F, RV32D, RV64F and RV64D.

--- c_emulator/riscv_sim.cpp
+++ c_emulator/riscv_sim.cpp
@@ -411,7 +411,7 @@ void init_sail_reset_vector(uint64_t entry)
   }
 
   /* boot at reset vector */
-  zPC = rom_base;
+  zforce_pc(rom_base);
 }
 
 void init_sail(uint64_t elf_entry)
@@ -427,15 +427,18 @@ void init_sail(uint64_t elf_entry)
     rv_clint_size = UINT64_C(0);
     rv_htif_tohost = UINT64_C(0);
     */
-    zPC = elf_entry;
-  } else
+    zforce_pc(elf_entry);
+  } else {
     init_sail_reset_vector(elf_entry);
+  }
 }
 
 /* reinitialize to clear state and memory, typically across tests runs */
 void reinit_sail(uint64_t elf_entry)
 {
   model_fini();
+  sail_set_abstract_xlen();
+  sail_set_abstract_ext_d_supported();
   model_init();
   init_sail(elf_entry);
 }
@@ -637,6 +640,8 @@ int main(int argc, char **argv)
 {
   int files_start = process_args(argc, argv);
 
+  sail_set_abstract_xlen();
+  sail_set_abstract_ext_d_supported();
   model_init();
 
   if (do_report_arch) {
