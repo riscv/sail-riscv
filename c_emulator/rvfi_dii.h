ec3fa86b162797039c9656113886f4da9cce031b
Mingzhu Yan <yanmingzhu@iscas.ac.cn>
2025-07-01 00:45:55 +0000

    Use byte index in comment

--- c_emulator/rvfi_dii.h
+++ c_emulator/rvfi_dii.h
@@ -9,65 +9,65 @@
 // ****************************************************************************
 
 struct RVFI_DII_Instruction_Packet {
-  // 31 ..  0, Instruction word: 32-bit instruction or command.
+  // [3 - 0], Instruction word: 32-bit instruction or command.
   // The lower 16-bits may decode to a 16-bit compressed instruction.
   uint32_t rvfi_insn = 0;
-  // 47 .. 32, Time to inject token.
+  // [5 - 4], Time to inject token.
   // The difference between this and the previous instruction time gives a delay
   // before injecting this instruction. This can be ignored for models but gives
   // repeatability for implementations while shortening counterexamples.
   uint16_t rvfi_time = 0;
-  // 55 .. 48, This token is a trace command.
+  // [6], This token is a trace command.
   // For example, reset device under test.
   uint8_t rvfi_cmd = 0;
-  // 63 .. 56
+  // [7]
   uint8_t padding = 0;
 
   static RVFI_DII_Instruction_Packet from_u64(uint64_t value);
 };
 
 struct RVFI_DII_Execution_Packet_InstMetadata {
-  // 63 .. 0, The rvfi_order field must be set to the instruction index. No
+  // [07 - 00], The rvfi_order field must be set to the instruction index. No
   // indices must be used twice and there must be no gaps. Instructions may be
   // retired in a reordered fashion, as long as causality is preserved
   // (register and memory write operations must be retired before the read
   // operations that depend on them).
   uint64_t rvfi_order = 0;
-  // 127 .. 64, rvfi_insn is the instruction word for the retired instruction.
+  // [15 - 08], rvfi_insn is the instruction word for the retired instruction.
   // In case of an instruction with fewer than ILEN bits, the upper bits of this
   // output must be all zero. For compressed instructions the compressed
   // instruction word must be output on this port. For fused instructions the
   // complete fused instruction sequence must be output.
   uint64_t rvfi_insn = 0;
-  // 135 .. 128, rvfi_trap must be set for an instruction that cannot be
+  // [16], rvfi_trap must be set for an instruction that cannot be
   // decoded as a legal instruction, such as 0x00000000. In addition, rvfi_trap
   // must be set for a misaligned memory read or write in PMAs that don't allow
   // misaligned access, or other memory access violations. rvfi_trap must also
   // be set for a jump instruction that jumps to a misaligned instruction.
   uint8_t rvfi_trap = 0;
-  // 143 .. 136, The signal rvfi_halt must be set when the instruction is the
+  // [17], The signal rvfi_halt must be set when the instruction is the
   // last instruction that the core retires before halting execution. It should
   // not be set for an instruction that triggers a trap condition if the CPU
   // reacts to the trap by executing a trap handler. This signal enables
   // verification of liveness properties.
   uint8_t rvfi_halt = 0;
-  // 151 .. 144, rvfi_intr must be set for the first instruction that is part
+  // [18], rvfi_intr must be set for the first instruction that is part
   // of a trap handler, i.e. an instruction that has a rvfi_pc_rdata that does
   // not match the rvfi_pc_wdata of the previous instruction.
   uint8_t rvfi_intr = 0;
-  // 159 .. 152, rvfi_mode must be set to the current privilege level, using
+  // [19], rvfi_mode must be set to the current privilege level, using
   // the following encoding: 0=U-Mode, 1=S-Mode, 2=Reserved, 3=M-Mode
   uint8_t rvfi_mode = 0;
-  // 167 .. 160, rvfi_ixl must be set to the value of MXL/SXL/UXL in the
+  // [20], rvfi_ixl must be set to the value of MXL/SXL/UXL in the
   // current privilege level, using the following encoding: 1=32, 2=64
   uint8_t rvfi_ixl = 0;
-  // 175 .. 168, When the core retires an instruction, it asserts the
+  // [21], When the core retires an instruction, it asserts the
   // rvfi_valid signal and uses the signals described below to output the
   // details of the retired instruction. The signals below are only valid during
   // such a cycle and can be driven to arbitrary values in a cycle in which
   // rvfi_valid is not asserted.
   uint8_t rvfi_valid = 0;
-  // 191 .. 176, Note: since we only send these packets in the valid state, we
+  // [23 - 22], Note: since we only send these packets in the valid state, we
   // could omit the valid signal, but we need 3 bytes of padding after ixl
   // anyway so we might as well include it.
   uint16_t padding = 0;
@@ -77,25 +77,27 @@ typedef struct {
   // This is the program counter (pc) before (rvfi_pc_rdata) and after
   // (rvfi_pc_wdata) execution of this instruction. I.e. this is the address
   // of the retired instruction and the address of the next instruction.
-  uint64_t rvfi_pc_rdata = 0; // 63 .. 0
-  uint64_t rvfi_pc_wdata = 0; // 127 .. 64
+  uint64_t rvfi_pc_rdata = 0; // [07 - 00]
+  uint64_t rvfi_pc_wdata = 0; // [15 - 08]
 } RVFI_DII_Execution_Packet_PC;
 
 #define MAGIC_INT_DATA {'i', 'n', 't', '-', 'd', 'a', 't', 'a'}
 
 struct RVFI_DII_Execution_Packet_Ext_Integer {
-  // 63 .. 0, must be "int-data"
+  // [07 - 00], must be "int-data"
   // rvfi_rd_wdata is the value of the x register addressed by rd after
   // execution of this instruction. This output must be zero when rd is zero.
   char magic[8] = MAGIC_INT_DATA;
-  // 127 ..  64
+  // [15 - 08]
   uint64_t rvfi_rd_wdata = 0;
   // rvfi_rs1_rdata/rvfi_rs2_rdata is the value of the x register addressed
   // by rs1/rs2 before execution of this instruction. This output must be
   // zero when rs1/rs2 is zero.
-  uint64_t rvfi_rs1_rdata = 0; // 191 .. 128
-  uint64_t rvfi_rs2_rdata = 0; // 255 .. 192
-  // 263 .. 256, rvfi_rd_addr is the decoded rd register address for the retired
+  // [23 - 16]
+  uint64_t rvfi_rs1_rdata = 0;
+  // [31 - 24]
+  uint64_t rvfi_rs2_rdata = 0;
+  // [32], rvfi_rd_addr is the decoded rd register address for the retired
   // instruction. For an instruction that writes no rd register, this output
   // must always be zero.
   uint8_t rvfi_rd_addr = 0;
@@ -104,38 +106,41 @@ struct RVFI_DII_Execution_Packet_Ext_Integer {
   // For an instruction that reads no rs1/rs2 register, this output can have an
   // arbitrary value. However, if this output is nonzero then rvfi_rs1_rdata
   // must carry the value stored in that register in the pre-state.
-  uint8_t rvfi_rs1_addr = 0; // 271 .. 264
-  uint8_t rvfi_rs2_addr = 0; // 279 .. 272
+  // [33]
+  uint8_t rvfi_rs1_addr = 0;
+  // [34]
+  uint8_t rvfi_rs2_addr = 0;
+  // [39 - 35]
   uint8_t padding[5] = {};
 };
 
 #define MAGIC_MEM_DATA {'m', 'e', 'm', '-', 'd', 'a', 't', 'a'}
 
 struct RVFI_DII_Execution_Packet_Ext_MemAccess {
-  // 63 .. 0, magic must be "mem-data"
+  // [07 - 00], magic must be "mem-data"
   char magic[8] = MAGIC_MEM_DATA;
-  // 319 ..  64
+  // [39 - 08]
   // rvfi_mem_rdata is the pre-state data read from rvfi_mem_addr.
   // rvfi_mem_rmask specifies which bytes are valid.
   // CHERI-extension: widened to 32 bytes to allow reporting 129 bits
   uint64_t rvfi_mem_rdata[4] = {};
-  // 575 .. 320
+  // [71 - 40]
   // rvfi_mem_wdata is the post-state data written to rvfi_mem_addr.
   // rvfi_mem_wmask specifies which bytes are valid.
   // CHERI-extension: widened to 32 bytes to allow reporting 129 bits
   uint64_t rvfi_mem_wdata[4] = {};
-  // 607 .. 576
+  // [75 - 72]
   // rvfi_mem_rmask is a bitmask that specifies which bytes in rvfi_mem_rdata
   // contain valid read data from rvfi_mem_addr.
   // CHERI-extension: we extend rmask+wmask to 32 bits to allow reporting the
   // mask for CHERI/RV128 accesses here.
   uint32_t rvfi_mem_rmask = 0;
-  // 639 .. 608
+  // [79 - 76]
   // rvfi_mem_wmask is a bitmask that specifies which bytes in rvfi_mem_wdata
   // contain valid data that is written to rvfi_mem_addr.
   // CHERI-extension: widened to 32 bits
   uint32_t rvfi_mem_wmask = 0;
-  // 703 .. 640
+  // [87 - 80]
   // For memory operations (rvfi_mem_rmask and/or rvfi_mem_wmask are non-zero),
   // rvfi_mem_addr holds the accessed memory location.
   uint64_t rvfi_mem_addr = 0;
@@ -154,26 +159,26 @@ struct RVFI_DII_Execution_Packet_Ext_MemAccess {
 // ****************************************************************************
 
 struct RVFI_DII_Execution_Packet_V1 {
-  // [00 - 07] Instruction number: INSTRET value after completion.
+  // [07 - 00] Instruction number: INSTRET value after completion.
   uint64_t rvfi_order = 0;
-  // [08 - 15] PC before instr: PC for current instruction.
+  // [15 - 08] PC before instr: PC for current instruction.
   uint64_t rvfi_pc_rdata = 0;
-  // [16 - 23] PC after instr: Following PC - either PC + 4 or
+  // [23 - 16] PC after instr: Following PC - either PC + 4 or
   // jump/trap target.
   uint64_t rvfi_pc_wdata = 0;
-  // [24 - 31] Instruction word: 32-bit command value.
+  // [31 - 24] Instruction word: 32-bit command value.
   uint64_t rvfi_insn = 0;
-  // [32 - 39] Read register values: Values as read from registers named.
+  // [39 - 32] Read register values: Values as read from registers named.
   uint64_t rvfi_rs1_data = 0;
-  // [40 - 47] above. Must be 0 if register ID is 0. 0 if unused.
+  // [47 - 40] above. Must be 0 if register ID is 0. 0 if unused.
   uint64_t rvfi_rs2_data = 0;
-  // [48 - 55] Write register value: MUST be 0 if rd_ is 0.
+  // [55 - 48] Write register value: MUST be 0 if rd_ is 0.
   uint64_t rvfi_rd_wdata = 0;
-  // [56 - 63] Memory access addr: Points to byte address (aligned if define.
+  // [63 - 56] Memory access addr: Points to byte address (aligned if define.
   uint64_t rvfi_mem_addr = 0;
-  // [64 - 71] Read data: Data read from mem_addr (i.e. before write).
+  // [71 - 64] Read data: Data read from mem_addr (i.e. before write).
   uint64_t rvfi_mem_rdata = 0;
-  // [72 - 79] Write data: Data written to memory by this command.
+  // [79 - 72] Write data: Data written to memory by this command.
   uint64_t rvfi_mem_wdata = 0;
   // [80] Read mask: Indicates valid bytes read. 0 if unused.
   uint8_t rvfi_mem_rmask = 0;
@@ -200,15 +205,15 @@ struct RVFI_DII_Execution_Packet_V1 {
 #define MAGIC_TRACE_V2 {'t', 'r', 'a', 'c', 'e', '-', 'v', '2'}
 
 struct RVFI_DII_Execution_Packet_V2 {
-  // 63 .. 0, must be set to 'trace-v2'
+  // [08 - 00], must be set to 'trace-v2'
   char magic[8] = MAGIC_TRACE_V2;
-  // 127 .. 64, total size of the trace packet + extensions
+  // [15 - 00], total size of the trace packet + extensions
   uint64_t trace_size = 0;
-  // 319 .. 128
+  // [39 - 16]
   RVFI_DII_Execution_Packet_InstMetadata basic_data = {};
-  // 447 .. 320
+  // [55 - 39]
   RVFI_DII_Execution_Packet_PC pc_data = {};
-  // 511 .. 448
+  // [63 - 55]
   uint64_t available_fields = 0;
   // 448, Followed by RVFI_DII_Execution_Packet_Ext_Integer if set
   // integer_data_available
