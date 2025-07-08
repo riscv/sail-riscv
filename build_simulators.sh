919555763fd62aac7105431cd3f1adc48273e2ec
Nadime Barhoumi <nadime@riscv.org>
2025-07-08 14:30:31 -0400

    Set ENABLE_RISCV_TESTS by default to off but enable it in CI and in shell script

--- build_simulators.sh
+++ build_simulators.sh
@@ -2,6 +2,7 @@
 
 set -e
 : "${DOWNLOAD_GMP:=TRUE}"
-cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDOWNLOAD_GMP="${DOWNLOAD_GMP}"
+: "${ENABLE_RISCV_TESTS:=TRUE}"
+cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDOWNLOAD_GMP="${DOWNLOAD_GMP}" -DENABLE_RISCV_TESTS="${ENABLE_RISCV_TESTS}"
 jobs=$( (nproc || sysctl -n hw.ncpu || echo 2) 2>/dev/null)
 cmake --build build -j${jobs}
