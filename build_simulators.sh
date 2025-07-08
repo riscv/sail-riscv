ae11a54f46c889866b5beca31c2d29a6427300e6
Nadime Barhoumi <nadime@riscv.org>
2025-07-08 08:57:08 -0400

    Remove set_tests_properties()

--- build_simulators.sh
+++ build_simulators.sh
@@ -2,6 +2,6 @@
 
 set -e
 : "${DOWNLOAD_GMP:=TRUE}"
-cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDOWNLOAD_GMP="${DOWNLOAD_GMP}" -DENABLE_RISCV_VECTOR_TESTS_V256_E32=TRUE
+cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDOWNLOAD_GMP="${DOWNLOAD_GMP}"
 jobs=$( (nproc || sysctl -n hw.ncpu || echo 2) 2>/dev/null)
 cmake --build build -j${jobs}
