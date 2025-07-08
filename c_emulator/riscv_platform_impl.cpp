c3a16b7a3255615b3b967eb65a2faf167bee12ba
KotorinMinami <huangshuo4@gmail.com>
2025-07-09 02:15:21 +0800

    replace rv_16_random_bits implemention with cpp std mt19937

--- c_emulator/riscv_platform_impl.cpp
+++ c_emulator/riscv_platform_impl.cpp
@@ -1,20 +1,13 @@
 #include "riscv_platform_impl.h"
 #include <unistd.h>
 #include <stdio.h>
+#include <random>
 
 // Provides entropy for the scalar cryptography extension.
 uint64_t rv_16_random_bits(void)
 {
-  // This function can be changed to support deterministic sequences of
-  // pseudo-random bytes. This is useful for testing.
-  const char *name = "/dev/urandom";
-  FILE *f = fopen(name, "rb");
-  uint16_t val;
-  if (fread(&val, 2, 1, f) != 1) {
-    fprintf(stderr, "Unable to read 2 bytes from %s\n", name);
-  }
-  fclose(f);
-  return (uint64_t)val;
+  static std::mt19937_64 rng(0);
+  return static_cast<uint16_t>(rng());
 }
 
 bool rv_enable_experimental_extensions = false;
