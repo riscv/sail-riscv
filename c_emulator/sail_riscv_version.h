37b6e65bf26f9ea607cf7604e0b00e48f61e3782
Prashanth Mundkur <103736+pmundkur@users.noreply.github.com>
2025-07-10 09:23:42 -0500

    struct -> namespace.

--- c_emulator/sail_riscv_version.h
+++ c_emulator/sail_riscv_version.h
@@ -2,9 +2,11 @@
 
 #include <string>
 
-struct version_info {
-  static const std::string release_version;
-  static const std::string git_version;
-  static const std::string sail_version;
-  static const std::string cxx_compiler_version;
+namespace version_info {
+
+static const std::string release_version;
+static const std::string git_version;
+static const std::string sail_version;
+static const std::string cxx_compiler_version;
+
 };
