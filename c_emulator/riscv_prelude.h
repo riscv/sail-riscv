913805bd768f5b08754eec915f83d708f5b24579
Mingzhu Yan <yanmingzhu@iscas.ac.cn>
2025-08-12 14:32:16 +0000

    Remove duplicate config_print check
    The config flags config_print_instr and config_print_platform are checked twice
    in the Sail and C code. This commit remove C function print_instr() and
    print_platform(), and replace them with print_log().

--- c_emulator/riscv_prelude.h
+++ c_emulator/riscv_prelude.h
@@ -9,9 +9,8 @@ extern "C" {
 
 unit print_string(sail_string prefix, sail_string msg);
 
-unit print_instr(sail_string s);
+unit print_log(sail_string s);
 unit print_step(unit);
-unit print_platform(sail_string s);
 
 bool get_config_print_instr(unit);
 bool get_config_print_platform(unit);
