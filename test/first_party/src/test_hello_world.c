56aacbd512ccbca252d0dfa3153c410c0ca8200d
Jordan Carlin <jordanmcarlin@gmail.com>
2025-08-22 12:00:14 -0700

    Add codespell to pre-commit hooks

--- test/first_party/src/test_hello_world.c
+++ test/first_party/src/test_hello_world.c
@@ -4,6 +4,7 @@
 
 int main()
 {
-  printf("Hello %s%c %d %u %f\n", "worl", 'd', 1, 2, 3.f);
+  printf("Hello %s%c %d %u %f\n", "worl", 'd', 1, 2, // codespell:ignore worl
+         3.f);
   return 0;
 }
