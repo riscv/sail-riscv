cbddfcd979ba6ad4e2c5abbacb20636e3e1f4916
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2025-09-07 07:36:05 -0500

    Consolidate the setting of abstract types in a single function. (#1260)
    It is too easy to miss adding each one in multiple places. This should
    go away once the compiler can generate them as part of `model_init()`.

--- c_emulator/config_utils.cpp
+++ c_emulator/config_utils.cpp
@@ -3,7 +3,19 @@
 #include <iostream>
 
 #include "sail_config.h"
+#include "sail_riscv_model.h"
 #include "config_utils.h"
+
+// These should eventually be part of compiler-generated `model_init()`,
+// but are consolidated here pending compiler support.
+void init_sail_configured_types()
+{
+  sail_set_abstract_xlen();
+  sail_set_abstract_vlen_exp();
+  sail_set_abstract_ext_d_supported();
+  sail_set_abstract_elen_exp();
+  sail_set_abstract_base_E_enabled();
+}
 
 uint64_t get_config_uint64(const std::vector<const char *> &keypath)
 {
--- c_emulator/config_utils.h
+++ c_emulator/config_utils.h
@@ -3,4 +3,6 @@
 #include <vector>
 #include "sail.h"
 
+void init_sail_configured_types();
+
 uint64_t get_config_uint64(const std::vector<const char *> &keypath);
