b521055ca4e94409a85047cf595551372bf226a7
Tim Hutt <timothy.hutt@codasip.com>
2025-09-09 09:00:57 +0100

    Make get_config_uint64 more flexible (#1253)
    Add support for reading bit vectors as well as integers. This will be
    needed in future to read the address that we should write the DTB
    (Device Tree Binary/Blob) to.

--- c_emulator/config_utils.h
+++ c_emulator/config_utils.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <cstdint>
 #include <vector>
-#include "sail.h"
 
 void init_sail_configured_types();
 
+// Get a uint64_t from the JSON config file. This works with integers and
+// non-abstract bit vectors. Bit vectors over 64 bits throw an exception.
+// Integers over 64 bits are currently silently truncated, and the sign
+// is ignored (-3 will be read as 3).
 uint64_t get_config_uint64(const std::vector<const char *> &keypath);
