c88b4b9234fc5223351652ee19a1494c571b91f0
Ariel Xiong <ArielHeleneto@outlook.com>
2025-11-07 16:44:39 +0800

    Remove extra semicolons.
    fix https://github.com/riscv/sail-riscv/pull/1372#discussion_r2499460267
    
    Signed-off-by: Ariel Xiong <ArielHeleneto@outlook.com>

--- c_emulator/riscv_callbacks_if.h
+++ c_emulator/riscv_callbacks_if.h
@@ -9,38 +9,38 @@ class callbacks_if {
 public:
   virtual ~callbacks_if() = default;
   // TODO: finding ways to improve the format
-  virtual void fetch_callback([[maybe_unused]] sbits opcode) { };
+  virtual void fetch_callback([[maybe_unused]] sbits opcode) { }
   virtual void mem_write_callback([[maybe_unused]] const char *type,
                                   [[maybe_unused]] sbits paddr,
                                   [[maybe_unused]] uint64_t width,
-                                  [[maybe_unused]] lbits value) { };
+                                  [[maybe_unused]] lbits value) { }
   virtual void mem_read_callback([[maybe_unused]] const char *type,
                                  [[maybe_unused]] sbits paddr,
                                  [[maybe_unused]] uint64_t width,
-                                 [[maybe_unused]] lbits value) { };
+                                 [[maybe_unused]] lbits value) { }
   virtual void
   mem_exception_callback([[maybe_unused]] sbits paddr,
-                         [[maybe_unused]] uint64_t num_of_exception) { };
+                         [[maybe_unused]] uint64_t num_of_exception) { }
   virtual void
   xreg_full_write_callback([[maybe_unused]] const_sail_string abi_name,
                            [[maybe_unused]] sbits reg,
-                           [[maybe_unused]] sbits value) { };
+                           [[maybe_unused]] sbits value) { }
   virtual void freg_write_callback([[maybe_unused]] unsigned reg,
-                                   [[maybe_unused]] sbits value) { };
+                                   [[maybe_unused]] sbits value) { }
   virtual void
   csr_full_write_callback([[maybe_unused]] const_sail_string csr_name,
                           [[maybe_unused]] unsigned reg,
-                          [[maybe_unused]] sbits value) { };
+                          [[maybe_unused]] sbits value) { }
   virtual void
   csr_full_read_callback([[maybe_unused]] const_sail_string csr_name,
                          [[maybe_unused]] unsigned reg,
-                         [[maybe_unused]] sbits value) { };
+                         [[maybe_unused]] sbits value) { }
   virtual void vreg_write_callback([[maybe_unused]] unsigned reg,
-                                   [[maybe_unused]] lbits value) { };
-  virtual void pc_write_callback([[maybe_unused]] sbits new_pc) { };
-  virtual void redirect_callback([[maybe_unused]] sbits new_pc) { };
+                                   [[maybe_unused]] lbits value) { }
+  virtual void pc_write_callback([[maybe_unused]] sbits new_pc) { }
+  virtual void redirect_callback([[maybe_unused]] sbits new_pc) { }
   virtual void trap_callback([[maybe_unused]] bool is_interrupt,
-                             [[maybe_unused]] fbits cause) { };
+                             [[maybe_unused]] fbits cause) { }
 };
 
 void register_callback(callbacks_if *cb);
