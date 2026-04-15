16cfb021c09dcddad72e6fb5be7a70d31068de0b
Leo Stefanesco <leo.lveb@gmail.com>
2026-04-15 13:47:27 +0100

    Lean: fix deprecation warning in handwritten support

--- handwritten_support/RiscvExtrasExecutable.lean
+++ handwritten_support/RiscvExtrasExecutable.lean
@@ -112,6 +112,6 @@ def riscv_f64roundToInt : BitVec 3 → BitVec 64 → Bool → (BitVec 5 × BitVe
 
 -- Termination of extensionEnabled
 instance : SizeOf extension where
-  sizeOf := extension.toCtorIdx
+  sizeOf := extension.ctorIdx
 
 macro_rules | `(tactic| decreasing_trivial) => `(tactic| decide)
