77b571e751c58832c80a0beaf244f2700c06cd89
Prashanth Mundkur <prashanth@riscv.org>
2026-06-30 08:49:23 -0500

    Enable some `readability` checks in `clang-tidy`.
    Other than some minor cleanup, this interestingly found a mismatch in a parameter name for `tlb_add_callback` and `tlb_flush_callback`, which used `index` in `riscv_platform_if.h` (correctly) and `level` in
    `riscv_platform_if.cpp` (incorrectly).

--- c_emulator/riscv_platform_if.cpp
+++ c_emulator/riscv_platform_if.cpp
@@ -117,7 +117,7 @@ unit PlatformInterface::ptw_fail_callback(
 
 unit PlatformInterface::tlb_add_callback(
   [[maybe_unused]] hart::zz5vecz8z5unionz0zzoptionzzIRTLB_EntryzzKz9 tlb,
-  [[maybe_unused]] uint64_t level
+  [[maybe_unused]] uint64_t index
 ) {
   return UNIT;
 }
@@ -126,7 +126,7 @@ unit PlatformInterface::tlb_flush_begin_callback(unit) {
   return UNIT;
 }
 
-unit PlatformInterface::tlb_flush_callback([[maybe_unused]] uint64_t level) {
+unit PlatformInterface::tlb_flush_callback([[maybe_unused]] uint64_t index) {
   return UNIT;
 }
 
