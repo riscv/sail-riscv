dcbd6c63d4d466700f8fa776b3a234736d4e4ebb
Brian Campbell <Brian.Campbell@ed.ac.uk>
2026-07-06 16:46:03 +0100

    Automatically fill in required Sail version in opam packaging

--- coq-sail-riscv.cmake
+++ coq-sail-riscv.cmake
@@ -1,7 +1,16 @@
-# This small script fills in the version number in the opam package file.
+# This small script fills in the version numbers in the opam package file.
 # It can be invoked using
 #
 #   cmake -P coq-sail-riscv.cmake
 
 include(cmake/project_version.cmake)
+include(cmake/sail_required_version.cmake)
+# If the Rocq output requires a more recent version of Sail than the model does,
+# you can override it here.
+if(EXISTS "cmake/sail_required_version_rocq.txt")
+  file(READ "cmake/sail_required_version_rocq.txt" sail_required_version_rocq)
+  string(STRIP "${sail_required_version_rocq}" sail_required_version_rocq)
+else()
+  set(sail_required_version_rocq ${SAIL_REQUIRED_VER})
+endif()
 configure_file("coq-sail-riscv.opam.in" "coq-sail-riscv.opam" @ONLY)
